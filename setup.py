4ac79c2297e21e793bca262c9931aca61b5daf73
ryokamoi <ryokamoi.jp@gmail.com>
2024-02-06 12:02:45 -0500

    updated setup.py - added sentencepiece for llama2

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(
     license="MIT",
     python_requires=">=3.9",
     install_requires=[
-        "openai==0.28",
-        "transformers==4.34.1", "easyllm==0.6.2", "langchain==0.0.350", "accelerate==0.25.0",  # llama2
+        "openai==0.28",  # openai models
+        "transformers==4.34.1", "easyllm==0.6.2", "langchain==0.0.350", "accelerate==0.25.0", "sentencepiece=0.1.99",  # llama2
         "easy_io @ git+ssh://git@github.com/ryokamoi/easy_io@0.2.1",
         "google-generativeai", "cohere"
     ]
